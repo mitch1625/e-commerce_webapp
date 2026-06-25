998757f6bd713d123625a54922bb8e42f3ab7163
eric <ericmitchell1625@hotmail.com>
2026-06-25 14:53:28 -0400

    updated APIs

--- front_end/src/utilities.jsx
+++ front_end/src/utilities.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const USER_API = process.env.REACT_APP_USER_SERVICE 
-const PRODUCTS_API = process.env.REACT_APP_PRODCUTS_SERVICE
-const CART_API = process.env.REACT_APP_CART_SERVICE
+const USER_API = import.meta.env.VITE_USER_SERVICE
+const PRODUCTS_API = import.meta.env.REACT_APP_PRODCUTS_SERVICE
+const CART_API = import.meta.env.REACT_APP_CART_SERVICE
 
 export const api = axios.create({
   baseURL: `${USER_API}/users`
@@ -11,7 +11,7 @@ export const productApi = axios.create({
 })
 
 export const cartApi = axios.create({
-  baseURL: `${CART_API}:8002/cart`
+  baseURL: `${CART_API}/cart`
 })
 
 export const convertApi = axios.create({
