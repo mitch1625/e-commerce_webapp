950608fad10e799c8cf947ee4ef2a5b52a8ce9e8
eric <ericmitchell1625@hotmail.com>
2026-06-25 14:33:33 -0400

    added live link to allow_origins, updated base APIs

--- back_end/user_service/main.py
+++ back_end/user_service/main.py
@@ -11,7 +11,10 @@ def create_app() -> FastAPI:
     app.include_router(auth_router)
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"], 
+        allow_origins=[
+        "https://e-commerce-webapp-3lmc.vercel.app/",
+        "http://localhost:5173",
+        ], 
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
