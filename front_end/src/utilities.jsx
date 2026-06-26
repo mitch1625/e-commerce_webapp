810dc05f8feb9c7236dab9b3dd7b15a1f11aa231
eric <ericmitchell1625@hotmail.com>
2026-06-26 09:31:54 -0400

    env var names fixed

--- front_end/src/utilities.jsx
+++ front_end/src/utilities.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const USER_API = import.meta.env.VITE_USER_SERVICE
-const PRODUCTS_API = import.meta.env.REACT_APP_PRODCUTS_SERVICE
-const CART_API = import.meta.env.REACT_APP_CART_SERVICE
+const PRODUCTS_API = import.meta.env.VITE_PRODCUTS_SERVICE
+const CART_API = import.meta.env.VITE_CART_SERVICE
 
 export const api = axios.create({
   baseURL: `${USER_API}/users`
