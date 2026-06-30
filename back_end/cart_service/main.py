514b2c35429782b4d5e3229a876ec4e76f9f048a
eric <ericmitchell1625@hotmail.com>
2026-06-30 10:40:07 -0400

    CORS updated to new prodlink

--- back_end/cart_service/main.py
+++ back_end/cart_service/main.py
@@ -11,7 +11,7 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
       allow_origins=[
-        "https://e-commerce-webapp-3lmc.vercel.app",
+        "https://e-commerce-webapp-bay.vercel.app/",
         "http://localhost:5173",
       ],
         allow_credentials=True,
